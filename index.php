87d025b579ee861e73e25e1e14145e6adfcd8df5
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2024-11-27 19:09:45 -0500

    feat: pruebas de cambios branchj

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    Esta es mi página web. Stalin Vasco prueba con load balancer ALB prueba de QA
+    Esta es mi página web. Stalin Vasco prueba con load balancer ALB prueba de QA branch
     <h2>Mi ip es: <?php echo $_SERVER['SERVER_ADDR']; ?></h2>
     <p>El hostname de esta instancia es: <?php echo gethostname(); ?></p>
   
